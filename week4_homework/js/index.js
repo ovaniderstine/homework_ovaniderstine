d0bd5d23cb9e5a6bceadabf9d54a16709ff23c45
ovaniderstine <oliviavaniderstine@gmail.com>
2017-02-22 00:09:20 -0800

    homework_week4

---
+++ week4_homework/js/index.js
@@ -0,0 +1,45 @@
+$(function(){
+    console.log("page loaded");
+    
+    $('#heading1').click(function(){
+        console.log("heading1 clicked");
+    });
+});
+console.log("file loaded");
+
+$(document).ready(function() {
+    // Configure/customize these variables.
+    var showChar = 100;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Show me more!";
+    var lesstext = "Show me less!!";
+    
+
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
